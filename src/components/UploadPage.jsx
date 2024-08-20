2d31af54c1bf964f61875b7144ccbef6f08d8c53
Nguyễn Võ Chí Dũng <145178800+NZodasic@users.noreply.github.com>
2024-08-20 12:51:25 +0700

    Update UploadPage.jsx

--- src/components/UploadPage.jsx
+++ src/components/UploadPage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 function UploadPage() {
     const [selectedFile, setSelectedFile] = useState(null);
+    const [result, setResult] = useState(null);  // Để lưu kết quả từ API
 
     const handleFileChange = (e) => {
         setSelectedFile(e.target.files[0]);
@@ -11,17 +12,18 @@ function UploadPage() {
         if (!selectedFile) return;
 
         const formData = new FormData();
-        formData.append('file', selectedFile);
+        formData.append('image', selectedFile); // Đổi 'file' thành 'image' để phù hợp với backend
 
         try {
-            const response = await fetch('http://nnquanghomeserver.ddnsking.com:5000', {
+            const response = await fetch('http://nnquanghomeserver.ddnsking.com:5000/upload_image', {
                 method: 'POST',
                 body: formData,
+                
             });
 
             if (response.ok) {
                 const data = await response.json();
-                console.log('Upload successful:', data);
+                setResult(data);  // Cập nhật kết quả nhận được từ API
             } else {
                 console.error('Upload failed');
             }
@@ -35,6 +37,15 @@ function UploadPage() {
             <h2>Upload Image</h2>
             <input type="file" onChange={handleFileChange} />
             <button onClick={handleUpload}>Upload</button>
+            
+            {result && (
+                <div>
+                    <h3>Prediction Result</h3>
+                    <p><strong>Best Class Name:</strong> {result.best_class_name}</p>
+                    <p><strong>Highest Confidence:</strong> {result.highest_confidence}</p>
+                    <img src={result.image_url} alt="Uploaded" style={{ width: '300px', height: 'auto' }} />
+                </div>
+            )}
         </div>
     );
 }
